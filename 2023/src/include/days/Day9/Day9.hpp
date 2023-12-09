0ce5f1d6b5f8b8c1bf8c87bc6c7a31dd6cf77013
coolguy1842 <coolio4691.gaming@gmail.com>
2023-12-09 22:43:49 +1000

    cleanup day 9 and speedup

--- 2023/src/include/days/Day9/Day9.hpp
+++ 2023/src/include/days/Day9/Day9.hpp
@@ -7,27 +7,24 @@
 #include <robin_hood.hpp>
 
 struct History {
-    std::vector<long> values;
-    std::vector<long> differences;
+    std::vector<std::vector<long>> values;
 
-    void setValues(std::vector<long> values) {
-        this->values = values;
-        this->updateDifferences();
+    void loadSubValues() {
+        std::vector<long> valuesList;
+
+        do {
+            const std::vector<long>& prevValues = values.back();
+            valuesList.clear();
+
+            for(size_t i = 1; i < prevValues.size(); i++) {
+                valuesList.push_back(prevValues[i] - prevValues[i - 1]);
+            }
+
+            values.push_back(valuesList);
+        } while(!isZero(valuesList));
     }
 
-    void updateDifferences() {
-        differences.clear();
-
-        if(values.size() <= 1) return;
-        long prevValue = values[0];
-
-        for(size_t i = 1; i < values.size(); i++) {
-            differences.push_back(values[i] - prevValue);
-            prevValue = values[i];
-        }
-    }
-
-    bool isZero() { return std::accumulate(values.begin(), values.end(), 0) == 0; }
+    bool isZero(std::vector<long> values) { return std::accumulate(values.begin(), values.end(), 0) == 0; }
 };
 
 class Day9 : public AOC::Day {
@@ -45,12 +42,14 @@ public:
 
             History history;
             long number;
+
+            history.values.push_back({});
             while(sscanf(current, "%ld %n", &number, &charsRead) != -1) {
-                history.values.push_back(number);
+                history.values[0].push_back(number);
                 current += charsRead;
             }
 
-            history.updateDifferences();
+            history.loadSubValues();
             historyList.push_back(history);
         }
     }
@@ -59,27 +58,15 @@ public:
         if(historyList.size() <= 0) loadHistory();
         partASolution = 0;
 
-        for(const History& history : historyList) {
-            std::vector<History> subHistoryList;
+        for(History& history : historyList) {
+            history.values.back().push_back(0);
 
-            History current = history;
-            while(!current.isZero()) {
-                History newHistory;
-                newHistory.setValues(current.differences);
-                subHistoryList.push_back(current);
-
-                current = newHistory;
+            for(long long i = (long long)history.values.size() - 2; i >= 0; i--) {
+                std::vector<long>& h = history.values[i];
+                h.push_back(h.back() + history.values[i + 1].back());
             }
 
-            subHistoryList.push_back(current);
-            (subHistoryList.end() - 1).base()->values.push_back(0);
-
-            for(long long i = (long long)subHistoryList.size() - 2; i >= 0; i--) {
-                History& sH = subHistoryList[i];
-                sH.values.push_back(*(sH.values.end() - 1) + *(subHistoryList[i + 1].values.end() - 1));
-            }
-
-            partASolution += *(subHistoryList.begin().base()->values.end() - 1);
+            partASolution += history.values.front().back();
         }
     }
 
@@ -87,28 +74,15 @@ public:
         if(historyList.size() <= 0) loadHistory();
         partBSolution = 0;
 
-        for(const History& history : historyList) {
-            std::vector<History> subHistoryList;
+        for(History& history : historyList) {
+            history.values.back().push_back(0);
 
-            History current = history;
-            while(!current.isZero()) {
-                History newHistory;
-                newHistory.setValues(current.differences);
-                subHistoryList.push_back(current);
-
-                current = newHistory;
+            for(long long i = (long long)history.values.size() - 2; i >= 0; i--) {
+                std::vector<long>& h = history.values[i];
+                h.insert(h.begin(), h.front() - history.values[i + 1].front());
             }
 
-            subHistoryList.push_back(current);
-            (subHistoryList.end() - 1).base()->values.push_back(0);
-
-            for(long long i = (long long)subHistoryList.size() - 2; i >= 0; i--) {
-                History& sH = subHistoryList[i];
-
-                sH.values.insert(sH.values.begin(), *(sH.values.begin()) - *(subHistoryList[i + 1].values.begin()));
-            }
-
-            partBSolution += *subHistoryList.begin().base()->values.begin();
+            partBSolution += history.values.front().front();
         }
     }
 
