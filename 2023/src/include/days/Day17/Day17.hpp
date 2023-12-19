e92ff1e2bfd50e7488655a768011d12f0f9d06c1
coolguy1842 <coolio4691.gaming@gmail.com>
2023-12-19 13:05:17 +1000

    update a bit

--- 2023/src/include/days/Day17/Day17.hpp
+++ 2023/src/include/days/Day17/Day17.hpp
@@ -13,59 +13,53 @@ struct position {
     size_t y;
         
     bool operator==(const position& pos) { return x == pos.x && y == pos.y; }
+    bool operator==(const position& pos) const { return x == pos.x && y == pos.y; }
 }; 
 
+
+struct position_hash {
+    inline std::size_t operator()(const position& c) const {
+        return (c.x << 32) + c.y;
+    }
+};
+
+
 struct cell {
-    struct position position;
+    struct dijkstra::position position;
     int heatLoss;
 
-    bool visited = false;
+    int lowest = __INT_MAX__;
 };
 
 std::array<cell*, 4> getValidNeighbours(position& pos, std::vector<dijkstra::cell*> prevCells, std::vector<std::vector<cell>>& grid, const size_t& width, const size_t& height) {
     std::array<cell*, 4> neighbours = {nullptr, nullptr, nullptr, nullptr};
+
+    size_t lastDirChange = prevCells.size();
+    for(long long i = (long long)prevCells.size() - 2; i > 0; i--) {
+        cell* right = prevCells[i + 1], *mid = prevCells[i], *left = prevCells[i - 1];
+        if(left->position.x != right->position.x && left->position.y != right->position.y) {
+            lastDirChange = prevCells.size() - i;
+            break;
+        }
+    }
+
+    if(lastDirChange <= 3) {
+        if(pos.x > 0) neighbours[0] = &grid[pos.y][pos.x - 1];
+        if(pos.x < width - 1) neighbours[1] = &grid[pos.y][pos.x + 1];
+
+        if(pos.y > 0) neighbours[2] = &grid[pos.y - 1][pos.x];
+        if(pos.y < height - 1) neighbours[3] = &grid[pos.y + 1][pos.x];
+
+        return neighbours;
+    }
+
+
+    cell* lastDirCell = prevCells[prevCells.size() - 2];
+    if(lastDirCell->position.y != pos.y && pos.x > 0) neighbours[0] = &grid[pos.y][pos.x - 1];
+    if(lastDirCell->position.y != pos.y && pos.x < width - 1) neighbours[1] = &grid[pos.y][pos.x + 1];
     
-    // if(prevCells.size() == 3) {
-    //     if(!(prevCells[prevCells.size() - 3]->position.y == pos.y &&
-    //             prevCells[prevCells.size() - 2]->position.y == pos.y &&
-    //             prevCells[prevCells.size() - 1]->position.y == pos.y)) {
-    //         if(pos.x > 0) neighbours[0] = &grid[pos.y][pos.x - 1];
-    //         if(pos.x < width - 1) neighbours[1] = &grid[pos.y][pos.x + 1];
-    //     }
-
-    //     if(!(prevCells[prevCells.size() - 3]->position.x == pos.x &&
-    //             prevCells[prevCells.size() - 2]->position.x == pos.x &&
-    //             prevCells[prevCells.size() - 1]->position.x == pos.x)) {
-    //         if(pos.y > 0) neighbours[2] = &grid[pos.y - 1][pos.x];
-    //         if(pos.y < height - 1) neighbours[3] = &grid[pos.y + 1][pos.x];
-    //     }
-
-    //     return neighbours;
-    // }
-    // else if(prevCells.size() >= 4) {
-    //     if(!(
-    //             prevCells[prevCells.size() - 3]->position.y == pos.y &&
-    //             prevCells[prevCells.size() - 2]->position.y == pos.y &&
-    //             prevCells[prevCells.size() - 1]->position.y == pos.y)) {
-    //         if(pos.x > 0) neighbours[0] = &grid[pos.y][pos.x - 1];
-    //         if(pos.x < width - 1) neighbours[1] = &grid[pos.y][pos.x + 1];
-    //     }
-
-    //     if(!(
-    //             prevCells[prevCells.size() - 3]->position.x == pos.x &&
-    //             prevCells[prevCells.size() - 2]->position.x == pos.x &&
-    //             prevCells[prevCells.size() - 1]->position.x == pos.x)) {
-    //         if(pos.y > 0) neighbours[2] = &grid[pos.y - 1][pos.x];
-    //         if(pos.y < height - 1) neighbours[3] = &grid[pos.y + 1][pos.x];
-    //     }
-
-    //     return neighbours;
-    // }
-
-    if(pos.x > 0) neighbours[0] = &grid[pos.y][pos.x - 1];
-    if(pos.x < width - 1) neighbours[1] = &grid[pos.y][pos.x + 1];
-    if(pos.y > 0) neighbours[2] = &grid[pos.y - 1][pos.x];
-    if(pos.y < height - 1) neighbours[3] = &grid[pos.y + 1][pos.x];
+    if(lastDirCell->position.x != pos.x && pos.y > 0) neighbours[2] = &grid[pos.y - 1][pos.x];
+    if(lastDirCell->position.x != pos.x && pos.y < height - 1) neighbours[3] = &grid[pos.y + 1][pos.x];
 
     return neighbours;
 }
@@ -76,11 +70,13 @@ int dijkstraSearch(struct std::vector<std::vector<dijkstra::cell>>& grid, const 
         int totalCost = 0;
         
         std::vector<dijkstra::cell*> prevCells;
+        robin_hood::unordered_flat_set<dijkstra::position, dijkstra::position_hash> travelled;
 
         bool operator>(const queueContents& qC) const { return totalCost > qC.totalCost; }
     };
 
-    std::priority_queue<queueContents, std::vector<queueContents>, std::greater<queueContents>> queue;
+    
+    /*std::priority_queue<queueContents, std::vector<queueContents>, std::greater<queueContents>> queue;
     
 
     struct position pos = start;
@@ -128,10 +124,75 @@ int dijkstraSearch(struct std::vector<std::vector<dijkstra::cell>>& grid, const 
             }
 
             queue.emplace(cell, totalCost + cell->heatLoss, prev);
+        }*/
+
+    std::priority_queue<queueContents, std::vector<queueContents>, std::greater<queueContents>> queue;
+    int end = maxScore;
+
+    queue.push({ &grid[start.y][start.x], 0, {} });
+
+    while (!queue.empty()) {
+        const struct queueContents cur = queue.top();
+        queue.pop();
+
+        auto newPrev = cur.prevCells;
+        auto newTravelled = cur.travelled;
+        newPrev.push_back(cur.cell);
+        newTravelled.emplace(cur.cell->position);
+
+        /*
+        for(size_t y : range(grid.size())) {
+            for(size_t x : range(grid[0].size())) {
+                bool found = false;
+                for(size_t c : range(newPrev.size())) {
+                    auto cur = newPrev[c];
+
+                    if(cur->position.x == x && cur->position.y == y) {
+                        if(c == 0) {
+                            printf("#");
+                            found = true;
+                            continue;
+                        }
+                        
+                        
+                        auto prev = newPrev[c - 1];
+
+                        if(cur->position.x > prev->position.x) printf(">");
+                        else if(cur->position.x < prev->position.x) printf("<");
+                        else if(cur->position.y > prev->position.y) printf("v");
+                        else if(cur->position.y < prev->position.y) printf("^");
+                        else {
+                            printf("#");
+                        }
+
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found) printf("%d", grid[y][x].heatLoss);
+            }
+
+            printf("\n");
+        }
+
+        printf("\n\n\n");
+        //*/
+        for (cell* cell : getValidNeighbours(cur.cell->position, newPrev, grid, grid[0].size(), grid.size())) {
+            if(cell == nullptr || newTravelled.contains(cell->position)) continue;
+
+            if(cell->position == dest) {
+                end = std::min(end, cur.totalCost + cell->heatLoss);
+                continue;
+            }
+            
+            newTravelled.emplace(cell->position);
+
+            queue.push({ cell, cur.totalCost + cell->heatLoss, newPrev, newTravelled });
         }
     }
 
-    return __INT_MAX__;
+    return end;
 }
 
 };
@@ -155,12 +216,8 @@ public:
                 cell.position = {x, y};
                 cell.heatLoss = heatLoss;
 
-                // printf("%d", heatLoss);
-
                 grid[y].push_back(cell);
-            }   
-
-            // printf("\n");
+            }
         }
     }
 
