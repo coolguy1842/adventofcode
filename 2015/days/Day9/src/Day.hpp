217207e05aac155715e96751bb2712cf858d3926
coolguy1842 <coolguy1842@proton.me>
2024-10-28 14:27:12 +1000

    optimizing is too much work so leave it

--- 2015/days/Day9/src/Day.hpp
+++ 2015/days/Day9/src/Day.hpp
@@ -19,6 +19,18 @@
 #endif
 
 
+typedef std::unordered_map<std::string, std::unordered_map<std::string, std::size_t>> TDistance;
+namespace Dijkstra {
+
+std::optional<size_t> getDistance(TDistance& distances, const std::string& from, const std::string& to) {
+    auto it = distances.find(from);
+    if(it == distances.end()) {
+        return std::nullopt;
+    }
+
+    return it->second[to];
+}
+
 struct cell {
     std::unordered_set<std::string> visited;
 
@@ -29,103 +41,98 @@ struct cell {
 };
 
 
+
+size_t runA(TDistance& distances, std::unordered_set<std::string>& names, size_t maxScore = std::numeric_limits<size_t>::max()) {
+    std::priority_queue<cell, std::vector<cell>, std::greater<cell>> queue;
+    queue.push({ {}, "", 0 });
+
+    while(!queue.empty()) {
+        cell curCell = queue.top();
+        queue.pop();
+
+        if(curCell.visited.contains(curCell.name)) {
+            continue;
+        }
+
+        if(curCell.visited.size() == names.size()) {
+            return curCell.cost;
+        }
+
+        curCell.visited.emplace(curCell.name);
+        for(std::string name : names) {
+            if(curCell.visited.contains(name)) {
+                continue;
+            }
+
+            std::optional<size_t> cellCost = getDistance(distances, curCell.name, name);
+            if(!cellCost.has_value()) {
+                continue;
+            }
+
+            cell newCell = {
+                curCell.visited,
+                name,
+                cellCost.value() + curCell.cost
+            };
+
+            queue.push(newCell);
+        }
+    }
+
+    return maxScore;
+}
+
+size_t runB(TDistance& distances, std::unordered_set<std::string>& names, size_t maxScore = std::numeric_limits<size_t>::max()) {
+    std::priority_queue<cell, std::vector<cell>, std::greater<cell>> queue;
+    queue.push({ {}, "", 0 });
+
+    size_t maxFound = 0;
+    while(!queue.empty()) {
+        cell curCell = queue.top();
+        queue.pop();
+
+        if(curCell.visited.contains(curCell.name)) {
+            continue;
+        }
+
+        if(curCell.visited.size() == names.size()) {
+            maxFound = std::max(curCell.cost, maxFound);
+            continue;
+        }
+
+        curCell.visited.emplace(curCell.name);
+        for(std::string name : names) {
+            if(curCell.visited.contains(name)) {
+                continue;
+            }
+
+            std::optional<size_t> cellCost = getDistance(distances, curCell.name, name);
+            if(!cellCost.has_value()) {
+                continue;
+            }
+
+            cell newCell = {
+                curCell.visited,
+                name,
+                cellCost.value() + curCell.cost
+            };
+
+            queue.push(newCell);
+        }
+    }
+
+    return maxFound;
+}
+};
+
+
+
 class Day : public AOCUtil::IDay {
 private:
-    typedef std::unordered_map<std::string, std::unordered_map<std::string, std::size_t>> TDistance;
     size_t aDistance;
     size_t bDistance;
 
-    std::optional<size_t> getDistance(TDistance& distances, const std::string& from, const std::string& to) {
-        auto it = distances.find(from);
-        if(it == distances.end()) {
-            return std::nullopt;
-        }
 
-        return it->second[to];
-    }
-
-    size_t dijkstra(TDistance& distances, std::unordered_set<std::string>& names, size_t maxScore = std::numeric_limits<size_t>::max()) {
-        std::priority_queue<cell, std::vector<cell>, std::greater<cell>> queue;
-        queue.push({ {}, "", 0 });
-
-        while(!queue.empty()) {
-            cell curCell = queue.top();
-            queue.pop();
-
-            if(curCell.visited.contains(curCell.name)) {
-                continue;
-            }
-
-            if(curCell.visited.size() == names.size()) {
-                return curCell.cost;
-            }
-
-            curCell.visited.emplace(curCell.name);
-            for(std::string name : names) {
-                if(curCell.visited.contains(name)) {
-                    continue;
-                }
-
-                std::optional<size_t> cellCost = getDistance(distances, curCell.name, name);
-                if(!cellCost.has_value()) {
-                    continue;
-                }
-
-                cell newCell = {
-                    curCell.visited,
-                    name,
-                    cellCost.value() + curCell.cost
-                };
-
-                queue.push(newCell);
-            }
-        }
-
-        return maxScore;
-    }
-
-    size_t dijkstraPartB(TDistance& distances, std::unordered_set<std::string>& names, size_t maxScore = std::numeric_limits<size_t>::max()) {
-        std::priority_queue<cell, std::vector<cell>, std::greater<cell>> queue;
-        // start on empty spot to allow for all positions
-        queue.push({ {}, "", 0 });
-
-        size_t maxFound = 0;
-        while(!queue.empty()) {
-            cell curCell = queue.top();
-            queue.pop();
-
-            if(curCell.visited.contains(curCell.name)) {
-                continue;
-            }
-
-            if(curCell.visited.size() == names.size()) {
-                maxFound = std::max(curCell.cost, maxFound);
-                continue;
-            }
-
-            curCell.visited.emplace(curCell.name);
-            for(std::string name : names) {
-                if(curCell.visited.contains(name)) {
-                    continue;
-                }
-
-                std::optional<size_t> cellCost = getDistance(distances, curCell.name, name);
-                if(!cellCost.has_value()) {
-                    continue;
-                }
-
-                cell newCell = {
-                    curCell.visited,
-                    name,
-                    cellCost.value() + curCell.cost
-                };
-
-                queue.push(newCell);
-            }
-        }
-
-        return maxFound;
-    }
 
 public:
     Day() : AOCUtil::IDay(INPUT_PATH), aDistance(0) {}
@@ -151,7 +158,7 @@ public:
             distances[""][name] = 0;
         }
 
-        aDistance = dijkstra(distances, names);
+        aDistance = Dijkstra::runA(distances, names);
     }
 
     void partB() {
@@ -175,7 +182,7 @@ public:
             distances[""][name] = 0;
         }
 
-        bDistance = dijkstraPartB(distances, names);
+        bDistance = Dijkstra::runB(distances, names);
     }
     
 
