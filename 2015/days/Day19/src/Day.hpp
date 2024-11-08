e29cc7e7ea9d865415c2ae26b18341f99ffcd51d
coolguy1842 <coolguy1842@proton.me>
2024-11-08 16:15:36 +1000

    day 19 and 20 done

--- 2015/days/Day19/src/Day.hpp
+++ 2015/days/Day19/src/Day.hpp
@@ -1,9 +1,12 @@
 #ifndef __DAY_HPP__
 #define __DAY_HPP__
 
-#include "scn/scan.h"
+#include <algorithm>
+#include <scn/scan.h>
 #include <IDay.hpp>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <spdlog/spdlog.h>
 #include <string>
 #include <unordered_set>
@@ -16,51 +19,94 @@
 
 struct Replacement {
     std::string original;
-    std::string result;
+    std::string replacement;
 };
 
 class Day : public AOCUtil::IDay {
 private:
     size_t aMoleculeCount;
+    size_t bMoleculeSteps;
 
-public:
-    Day() : AOCUtil::IDay(INPUT_PATH), aMoleculeCount(0) {}
+    std::vector<Replacement> initReplacements(const std::vector<std::string>& splitLines) {
+        std::vector<Replacement> out;
 
-    void partA() {
-        std::unordered_set<std::string> molecules;
-
-        // original, replacement
-        std::vector<Replacement> replacements;
-        std::string sequence;
-
-        std::vector<std::string>& splitLines = input.getSplitText("\n");
         for(size_t i = 0; i < splitLines.size() - 2; i++) {
-            std::string& line = splitLines[i];
+            const std::string& line = splitLines[i];
 
             auto result = scn::scan<std::string, std::string>(line, "{} => {}");
             auto [ original, replacement ] = result->values();
 
-            replacements.push_back({
+            out.push_back({
                 original,
                 replacement
             });
         }
 
-        sequence = splitLines[splitLines.size() - 1];
+        return out;
+    }
 
-        for(const Replacement& replacement : replacements) {            
-            while(true) {
-                std::string copy = sequence;
-                
+    inline bool matchStrings(const char* first, const char* second) {
+        while(*second != '\0') {
+            if(*first++ != *second++) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+public:
+    Day() : AOCUtil::IDay(INPUT_PATH), aMoleculeCount(0), bMoleculeSteps(0) {}
+
+    void partA() {
+        std::vector<std::string>& splitLines = input.getSplitText("\n");
+        std::vector<Replacement> replacements = initReplacements(splitLines);
+        
+        std::unordered_set<std::string> molecules;
+        const std::string& molecule = splitLines[splitLines.size() - 1];
+
+        for(const Replacement& replacement : replacements) {
+            for(size_t i = 0; i < molecule.size(); i++) {
+                if(matchStrings(molecule.c_str() + i, replacement.original.c_str())) {
+                    molecules.emplace(molecule.substr(0, i) + replacement.replacement + molecule.substr(i + replacement.original.size()));
+                }
+            }
+        }
+
+        aMoleculeCount = molecules.size();
+    }
+
+    void partB() {
+        std::vector<std::string>& splitLines = input.getSplitText("\n");
+        std::vector<Replacement> replacements = initReplacements(splitLines);
+
+        // reversing all the strings is faster
+        for(Replacement& replacement : replacements) {
+            std::reverse(replacement.original.begin(), replacement.original.end());
+            std::reverse(replacement.replacement.begin(), replacement.replacement.end());
+        }
+        
+        std::string cur = splitLines[splitLines.size() - 1];
+        std::reverse(cur.begin(), cur.end());
+
+        while(cur != "e") {
+            for(size_t i = 0; i < cur.size(); i++) {
+                for(const Replacement& replacement : replacements) {
+                    if(matchStrings(cur.c_str() + i, replacement.replacement.c_str())) {
+                        cur = cur.substr(0, i) + replacement.original + cur.substr(i + replacement.replacement.size());
+                        bMoleculeSteps++;
+
+                        break;
+                    }
+                }
             }
         }
     }
-
-    void partB() { }
     
 
     void printResults(bool partA, bool partB) {
         if(partA) spdlog::info("Part A: {}", aMoleculeCount);
+        if(partB) spdlog::info("Part B: {}", bMoleculeSteps);
     }
 };
 
