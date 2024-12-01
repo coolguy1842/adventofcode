c7bc860cbdee593e5be94e06457f92c317c6281b
Cool Guy <coolguy1842@proton.me>
2024-12-01 16:18:27 +1000

    cleanup unused/unnecessary code

--- 2024/days/Day1/src/Day.cpp
+++ 2024/days/Day1/src/Day.cpp
@@ -1,6 +1,5 @@
 #include <DayInput.hpp>
 #include <Day.hpp>
-#include <algorithm>
 #include <spdlog/spdlog.h>
 #include <unordered_map>
 #include <vector>
@@ -10,7 +9,6 @@ Day::Day() : AOCUtil::IDay(dayInput) {}
 size_t aSolution = 0;
 void Day::partA() {
     std::vector<size_t> leftList, rightList;
-    std::vector<size_t> leftListSorted, rightListSorted;
 
     for(std::string& line : input.getSplitText("\n")) {
         size_t left, right;
@@ -20,39 +18,28 @@ void Day::partA() {
         rightList.push_back(right);
     }
 
-    leftListSorted = std::vector<size_t>(leftList.begin(), leftList.end()); std::sort(leftListSorted.begin(), leftListSorted.end());
-    rightListSorted = std::vector<size_t>(rightList.begin(), rightList.end()); std::sort(rightListSorted.begin(), rightListSorted.end());
+    std::sort(leftList.begin(), leftList.end());
+    std::sort(rightList.begin(), rightList.end());
 
     for(size_t i = 0; i < leftList.size(); i++) {
-        auto& left = leftListSorted[i], &right = rightListSorted[i];
-        
-        aSolution += std::abs((long long)left - (long long)right);
+        aSolution += std::abs((long long)leftList[i] - (long long)rightList[i]);
     }
 }
 
 size_t bSolution = 0;
 void Day::partB() {
-    std::vector<size_t> leftList, rightList;
+    std::vector<size_t> leftList;
     std::unordered_map<size_t, size_t> frequency;
 
     for(std::string& line : input.getSplitText("\n")) {
         size_t left, right;
-
         sscanf(line.c_str(), "%zu   %zu", &left, &right);
+        
         leftList.push_back(left);
-        rightList.push_back(right);
+        frequency[right]++;
     }
 
-    for(size_t i : rightList) {
-        if(!frequency.contains(i)) {
-            frequency[i] = 1;
-            continue;
-        }
-
-        frequency[i]++;
-    }
-
-    for(size_t i : leftList) {
+    for(const size_t& i : leftList) {
         bSolution += i * frequency[i];
     }
 }
