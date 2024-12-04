db58ffd27478bde45decd1c3fe22f217fa6e0a98
Cool Guy <coolguy1842@proton.me>
2024-12-04 19:15:05 +1000

    optimize and cleanup a little, will try swapping to only use a string without split later

--- 2024/days/Day4/src/Day.cpp
+++ 2024/days/Day4/src/Day.cpp
@@ -6,83 +6,32 @@
 
 Day::Day() : AOCUtil::IDay(dayInput) {}
 
-#define SEQUENCE "XMAS"
-
-// must be in the sequence var
-char nextInSequence(char c) {
-    char temp[2];
-    temp[0] = c;
-    temp[1] = '\0';
-
-    const char* out = strstr(SEQUENCE, temp);
-    if(out == NULL) return '\0';
-    else return *(out + 1);
-}
-
-bool isOutOfRange(const std::vector<std::string>& grid, long x, long y) {
+bool isOutOfRange(const std::vector<std::string>& grid, const long& x, const long& y) {
     return x < 0 || y < 0 || x >= grid[0].size() || y >= grid.size();
-}
-
-
-enum Direction {
-    NONE = 0,
-    UP = 1 << 0,
-    DOWN = 1 << 1,
-    LEFT = 1 << 2,
-    RIGHT = 1 << 3
-};
-
-// starting x, y of the sequence, returns a vector as you may have multiple xmas' branched out from a start
-size_t get_sequence(const std::vector<std::string>& grid, long x, long y, size_t direction = Direction::NONE) {
-    const char& startChar = grid[y][x];
-    const char& nextChar = nextInSequence(startChar);
-
-    if(startChar == 'S') {
-        return 1;
-    }
-
-    if(direction == NONE) {
-        size_t out = 0;
-        for(long i = 0; i < 9; i++) {
-            long nextX = x + ((i % 3) - 1), nextY = y + ((i / 3) - 1);
-            
-            if(!isOutOfRange(grid, nextX, nextY) && grid[nextY][nextX] == nextChar) {
-                size_t dir = 0;
-                if(nextX < x) dir |= Direction::LEFT;
-                else if(nextX > x) dir |= Direction::RIGHT;
-
-                if(nextY < y) dir |= Direction::UP;
-                else if(nextY > y) dir |= Direction::DOWN;
-
-                out += get_sequence(grid, nextX, nextY, dir);
-            }
-        }
-
-        return out;
-    }
-    
-    long nextX = x, nextY = y;
-    if(direction & DOWN) nextY++;
-    else if(direction & UP) nextY--;
-
-    if(direction & RIGHT) nextX++;
-    else if(direction & LEFT) nextX--;
-
-    if(!isOutOfRange(grid, nextX, nextY) && grid[nextY][nextX] == nextChar) {
-        return get_sequence(grid, nextX, nextY, direction);
-    }
-
-    return 0;
 }
 
 size_t aSolution = 0;
 void Day::partA() {
     std::vector<std::string> grid = input.getSplitText("\n");
 
-    for(long y = 0; y < grid.size(); y++) {
-        for(long x = 0; x < grid[0].size(); x++) {
-            if(grid[y][x] == 'X') {
-                aSolution += get_sequence(grid, x, y);
+    for(long i = 0; i < grid.size(); i++) {
+        for(long j = 0; j < grid[0].size(); j++) {
+            if(grid[i][j] != 'X') {
+                continue;
+            }
+
+            for(long k = 0; k < 9; k++) {
+                long dX = (k % 3) - 1, dY = (k / 3) - 1;
+                
+                if( 
+                    k != 4 &&
+                    !isOutOfRange(grid, j + (dX * 3), i + (dY * 3)) &&
+                    grid[i + (dY * 1)][j + (dX * 1)] == 'M' &&
+                    grid[i + (dY * 2)][j + (dX * 2)] == 'A' &&
+                    grid[i + (dY * 3)][j + (dX * 3)] == 'S'
+                ) {
+                    aSolution++;
+                }
             }
         }
     }
@@ -95,27 +44,13 @@ void Day::partB() {
     for(long y = 1; y < grid.size() - 1; y++) {
         for(long x = 1; x < grid[0].size() - 1; x++) {
             if(grid[y][x] == 'A') {
+                char &topLeft = grid[y - 1][x - 1], topRight = grid[y - 1][x + 1];
+                char &bottomLeft = grid[y + 1][x - 1], bottomRight = grid[y + 1][x + 1];
+
                 if(
-                    grid[y + 1][x - 1] == 'M' && grid[y + 1][x + 1] == 'S' &&
-                    grid[y - 1][x - 1] == 'M' && grid[y - 1][x + 1] == 'S'
-                ) {
-                    bSolution++;
-                }
-                else if(
-                    grid[y + 1][x - 1] == 'S' && grid[y + 1][x + 1] == 'M' &&
-                    grid[y - 1][x - 1] == 'S' && grid[y - 1][x + 1] == 'M'
-                ) {
-                    bSolution++;
-                }
-                else if(
-                    grid[y + 1][x - 1] == 'M' && grid[y + 1][x + 1] == 'M' &&
-                    grid[y - 1][x - 1] == 'S' && grid[y - 1][x + 1] == 'S'
-                ) {
-                    bSolution++;
-                }
-                else if(
-                    grid[y + 1][x - 1] == 'S' && grid[y + 1][x + 1] == 'S' &&
-                    grid[y - 1][x - 1] == 'M' && grid[y - 1][x + 1] == 'M'
+                    topLeft != bottomRight &&
+                    (topLeft == 'M' || topLeft == 'S') && (bottomRight == 'M' || bottomRight == 'S') &&
+                    ((topLeft == topRight && bottomLeft == bottomRight) || (topLeft == bottomLeft && topRight == bottomRight))
                 ) {
                     bSolution++;
                 }
