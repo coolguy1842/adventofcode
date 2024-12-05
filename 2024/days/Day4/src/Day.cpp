b454ebe518dcf23d3c86b9f37b216e45183fb592
Cool Guy <coolguy1842@proton.me>
2024-12-05 12:34:27 +1000

    part b big speedup

--- 2024/days/Day4/src/Day.cpp
+++ 2024/days/Day4/src/Day.cpp
@@ -2,16 +2,18 @@
 #include <Day.hpp>
 #include <cstring>
 #include <spdlog/spdlog.h>
-#include <vector>
+
+// both use these variables so just define in one macro
+#define INIT_VARS() \
+    const size_t& strSize = input.text.size(); \
+    const char *strPtr = input.text.c_str(), *ptr = strPtr - 1; \
+    long width = strstr(strPtr, "\n") - strPtr;
 
 Day::Day() : AOCUtil::IDay(dayInput) {}
 
 size_t aSolution = 0;
 void Day::partA() {
-    const size_t& strSize = input.text.size();
-    
-    const char *strPtr = input.text.c_str(), *ptr = strPtr - 1;
-    long width = strstr(strPtr, "\n") - strPtr;
+    INIT_VARS()
     
     while((ptr = strstr(ptr + 1, "X"))) {
         size_t i = ptr - strPtr;
@@ -36,22 +38,23 @@ void Day::partA() {
 
 size_t bSolution = 0;
 void Day::partB() {
-    std::vector<std::string> grid = input.getSplitText("\n");
+    INIT_VARS()
+    
+    while((ptr = strstr(ptr + 1, "A"))) {
+        size_t i = ptr - strPtr;
+        if(i <= width || i - (strSize - width) <= width) {
+            continue;
+        }
 
-    for(long y = 1; y < grid.size() - 1; y++) {
-        for(long x = 1; x < grid[0].size() - 1; x++) {
-            if(grid[y][x] == 'A') {
-                char &topLeft = grid[y - 1][x - 1], topRight = grid[y - 1][x + 1];
-                char &bottomLeft = grid[y + 1][x - 1], bottomRight = grid[y + 1][x + 1];
+        const char &topLeft = strPtr[i - width - 2], topRight = strPtr[i -  width];
+        const char &bottomLeft = strPtr[i + width], bottomRight = strPtr[i + width + 2];
 
-                if(
-                    topLeft != bottomRight &&
-                    (topLeft == 'M' || topLeft == 'S') && (bottomRight == 'M' || bottomRight == 'S') &&
-                    ((topLeft == topRight && bottomLeft == bottomRight) || (topLeft == bottomLeft && topRight == bottomRight))
-                ) {
-                    bSolution++;
-                }
-            }
+        if(
+            topLeft != bottomRight &&
+            (topLeft == 'M' || topLeft == 'S') && (bottomRight == 'M' || bottomRight == 'S') &&
+            ((topLeft == topRight && bottomLeft == bottomRight) || (topLeft == bottomLeft && topRight == bottomRight))
+        ) {
+            bSolution++;
         }
     }
 }
