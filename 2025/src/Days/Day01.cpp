fd60445f12ede874cdc2900983295c0596a7d7e7
coolguy1842 <coolguy1842@proton.me>
2025-12-02 13:15:12 +1000

    cleanup day 1 and make parsing faster

--- 2025/src/Days/Day01.cpp
+++ 2025/src/Days/Day01.cpp
@@ -1,34 +1,40 @@
 #include <Days/Day01.hpp>
 #include <Input/Day01.hpp>
 #include <Util/StringUtil.hpp>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
 
 constexpr int32_t dialMax = 100;
 void Day1::partA() {
-    int32_t dial = 50;
-    StringUtil::split(input, "\n", [this, &dial](const std::string& line) {
-        dial += atoi(line.c_str() + 1) * (line[0] == 'L' ? -1 : 1);
+    const char* str = input.c_str();
+    char* end;
 
+    int32_t dial = 50;
+    do {
+        dial += strtol(str + 1, &end, 10) * (str[0] == 'R' ? 1 : -1);
         if(dial % dialMax == 0) {
             aSolution++;
         }
-    });
+
+        str = end + 1;
+    } while(*end);
 }
 
 void Day1::partB() {
+    const char* str = input.c_str();
+    char* end;
+
     int32_t dial = 50;
-    StringUtil::split(input, "\n", [this, &dial](const std::string& line) {
-        int32_t dir    = line[0] == 'L' ? -1 : 1;
-        int32_t target = dial + (atoi(line.c_str() + 1) * dir);
+    do {
+        int8_t dir     = str[0] == 'R' ? 1 : -1;
+        int32_t target = dial + (strtol(str + 1, &end, 10) * dir);
 
         for(; dial != target; dial += dir) {
             if(dial % dialMax == 0) {
                 bSolution++;
             }
         }
-    });
+
+        str = end + 1;
+    } while(*end);
 }
 
 void Day1::printSolutions(std::bitset<sizeof(IDay::SolutionFlags)> parts) {
