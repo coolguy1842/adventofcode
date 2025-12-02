01c1dfd9c46dad51f75782841169783eb69496f2
coolguy1842 <coolguy1842@proton.me>
2025-12-02 16:40:21 +1000

    optimize a lot, debug went from about 1s to 200ms part 1

--- 2025/src/Days/Day02.cpp
+++ 2025/src/Days/Day02.cpp
@@ -1,71 +1,65 @@
 #include <Days/Day02.hpp>
 #include <Input/Day02.hpp>
 #include <Util/StringUtil.hpp>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
 #include <string>
 
-void Day2::partA() {
-    auto isInvalid = [](uint64_t id) {
-        std::string str = std::to_string(id);
+bool isValidA(uint64_t id) {
+    std::string str = std::to_string(id);
+    // if odd then it cant be invalid
+    if(str.size() & 1) {
+        return true;
+    }
 
-        for(size_t i = 1; i <= str.size() / 2; i++) {
-            if(str.substr(0, i) == str.substr(i, str.size())) {
-                return true;
+    return strncmp(str.c_str(), str.c_str() + (str.size() / 2), str.size() / 2) != 0;
+}
+
+bool isValidB(uint64_t id) {
+    std::string str = std::to_string(id);
+
+    for(size_t i = 1; i <= str.size() / 2; i++) {
+        for(size_t j = i; j < str.size(); j += i) {
+            if(strncmp(str.c_str(), str.c_str() + j, i) != 0) {
+                goto valid;
             }
         }
 
         return false;
-    };
+    valid:
+    }
 
-    StringUtil::split(input, ",", [this, &isInvalid](std::string range) {
-        uint64_t start, end;
-        sscanf(range.c_str(), "%lu-%lu", &start, &end);
-
-        for(uint64_t id = start; id <= end; id++) {
-            if(isInvalid(id)) {
-                aSolution += id;
-            }
-        }
-    });
+    return true;
 }
 
-void Day2::partB() {
-    auto isInvalid = [](uint64_t id) {
-        std::string str = std::to_string(id);
+uint64_t runPart(const std::function<bool(uint64_t)>& func) {
+    uint64_t out = 0;
 
-        for(size_t i = 1; i <= str.size() / 2; i++) {
-            std::string first = str.substr(0, i);
+    const char* str = input.c_str() - 1;
+    char* endPtr;
 
-            bool invalid = true;
-            for(size_t j = i; j < str.size(); j += i) {
-                std::string second = str.substr(j, i);
-
-                if(first != second) {
-                    invalid = false;
-                    break;
-                }
-            }
-
-            if(invalid) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    StringUtil::split(input, ",", [this, &isInvalid](std::string range) {
-        uint64_t start, end;
-        sscanf(range.c_str(), "%lu-%lu", &start, &end);
+    do {
+        uint64_t start = std::strtoul(str + 1, &endPtr, 10);
+        uint64_t end   = std::strtoul(endPtr + 1, &endPtr, 10);
 
         for(uint64_t id = start; id <= end; id++) {
-            if(isInvalid(id)) {
-                bSolution += id;
+            if(!func(id)) {
+                out += id;
             }
         }
-    });
+
+        str = endPtr;
+    } while(*str);
+
+    return out;
 }
+
+void Day2::partA() { aSolution = runPart(isValidA); }
+void Day2::partB() { bSolution = runPart(isValidB); }
 
 void Day2::printSolutions(std::bitset<sizeof(IDay::SolutionFlags)> parts) {
     if(parts.test(A)) {
