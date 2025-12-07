cf9a9f2d5e31d821d54e721354238e9d47a381cf
coolguy1842 <coolguy1842@proton.me>
2025-12-07 15:00:44 +1000

    optimize day 6

--- 2025/src/Days/Day06.cpp
+++ 2025/src/Days/Day06.cpp
@@ -1,31 +1,35 @@
 #include <Days/Day06.hpp>
 #include <Input/Day06.hpp>
 #include <Util/StringUtil.hpp>
-#include <cctype>
-#include <cstddef>
-#include <cstdint>
-#include <cstdlib>
 #include <cstring>
-#include <string>
-#include <vector>
+
+constexpr size_t lines = 4;
 
 void Day6::partA() {
-    std::vector<std::string> split = StringUtil::split(input, '\n');
-    std::vector<std::vector<uint64_t>> numbers(StringUtil::split(split[0], ' ').size());
+    const size_t width = (strchrnul(input.c_str(), '\n') - input.c_str()) + 1;
 
-    for(size_t i = 0; i < split.size() - 1; i++) {
-        std::vector<std::string> splitNumbers = StringUtil::split(split[i], ' ');
-        for(size_t j = 0; j < splitNumbers.size(); j++) {
-            numbers[j].push_back(strtoul(splitNumbers[j].c_str(), NULL, 10));
-        }
-    }
+    for(const char* str = input.c_str() + (width * lines); str != NULL;) {
+        const char* end  = strpbrk(str + 1, "+*");
+        const size_t len = end != nullptr ? (end - str) - 1 : strlen(str);
 
-    std::vector<std::string> operations = StringUtil::split(split.back(), ' ');
-    for(size_t i = 0; i < operations.size(); i++) {
-        const char& operation = operations[i][0];
+        const char& operation = *str;
         uint64_t out          = (operation == '*' ? 1 : 0);
 
-        for(const uint64_t& num : numbers[i]) {
+        for(size_t num = 0, row = lines; row > 0; row--, num = 0) {
+            for(size_t col = 0; col <= len; col++) {
+                const char& c = str[(-row * width) + col];
+                if(!isdigit(c)) {
+                    continue;
+                }
+
+                num *= 10;
+                num += c - '0';
+            }
+
+            if(num == 0) {
+                continue;
+            }
+
             switch(operation) {
             case '+': out += num; break;
             case '*': out *= num; break;
@@ -33,31 +37,23 @@ void Day6::partA() {
         }
 
         aSolution += out;
+        str = end;
     }
 }
 
 void Day6::partB() {
-    std::vector<std::string> split = StringUtil::split(input, '\n');
+    const size_t width = (strchrnul(input.c_str(), '\n') - input.c_str()) + 1;
 
-    std::string operations = split.back();
-
-    const char* str = operations.c_str();
-    size_t offset   = 0;
-    while(str != nullptr) {
-        const char* end = strpbrk(str + 1, "+*");
-        size_t len      = end != nullptr ? (end - str) - 1 : strlen(str);
+    for(const char* str = input.c_str() + (width * lines); str != NULL;) {
+        const char* end  = strpbrk(str + 1, "+*");
+        const size_t len = end != nullptr ? (end - str) - 1 : strlen(str);
 
         const char& operation = *str;
         uint64_t out          = (operation == '*' ? 1 : 0);
 
-        for(size_t row = 0; row <= len; row++) {
-            uint64_t num = 0;
-            for(size_t col = 0; col < split.size() - 1; col++) {
-                if(split[col].size() <= row + offset) {
-                    break;
-                }
-
-                const char& c = split[col][row + offset];
+        for(size_t num = 0, row = 0; row <= len; row++, num = 0) {
+            for(size_t col = lines; col > 0; col--) {
+                const char& c = str[(-col * width) + row];
                 if(!isdigit(c)) {
                     continue;
                 }
@@ -77,9 +73,7 @@ void Day6::partB() {
         }
 
         bSolution += out;
-
         str = end;
-        offset += len + 1;
     }
 }
 
